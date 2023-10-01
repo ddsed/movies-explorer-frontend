1d6816f95569d0fb63b73ff836eab85632d5eb25
Dasha Sedova <dashassed@gmail.com>
2023-10-02 00:28:38 +0200

    feat: BurgerMenu, ReadMe

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import burgerMenu from '../../images/burger-menu.svg';
+import burgerMenuClose from '../../images/button-close-burger.svg';
 import './NavBar.css';
 
 function NavBar({loggedIn}) {
+  const location = useLocation();
+
+  const [isBurgerMenuOpen, setIsBurgerMenu] = React.useState(false);
+
+  function openBurgerMenu() {
+    setIsBurgerMenu(true)
+  }
+
+  function closeBurgerMenu() {
+    setIsBurgerMenu(false)
+  }
+
   return (
     <nav className="navbar">
       { 
@@ -14,7 +27,20 @@ function NavBar({loggedIn}) {
           <NavLink to="/saved-movies" className="navbar__link-saved-movies">Сохраненные фильмы</NavLink>
         </div>
         <NavLink to="/profile" className="navbar__link-profile">Аккаунт</NavLink>
-        <img src={burgerMenu} className='navbar__burger-menu' alt='Иконка меню' />
+
+        <aside className={`navbar__burger-menu ${isBurgerMenuOpen ? "navbar__burger-menu_opened" : ""}`}>
+          <div className="navbar__burger-menu-container">
+            <img src={burgerMenuClose } className="navbar__close-button" alt="Иконка закрытия бокового меню" onClick={closeBurgerMenu} />
+            <div className="navbar__burger-menu-links">
+                <NavLink to="/" className={`navbar__burger-menu-link ${location.pathname === "/" ? "navbar__burger-menu-link_active" : ''}`} onClick={closeBurgerMenu}>Главная</NavLink>
+                <NavLink to="/movies" className={`navbar__burger-menu-link ${location.pathname === "/movies" ? "navbar__burger-menu-link_active" : ''}`} onClick={closeBurgerMenu}>Фильмы</NavLink>
+                <NavLink to="/saved-movies" className={`navbar__burger-menu-link ${location.pathname === "/saved-movies" ? "navbar__burger-menu-link_active" : ''}`} onClick={closeBurgerMenu}>Сохранённые фильмы</NavLink>
+            </div>
+            <NavLink to="/profile" className="navbar__burger-menu-profile" onClick={closeBurgerMenu}>Аккаунт</NavLink>
+          </div>
+        </aside>
+
+        <img src={burgerMenu} className="navbar__burger-menu-icon" alt="Иконка меню" onClick={openBurgerMenu}/>
         </>
         : 
         <>
